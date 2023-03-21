c8020c488f2f385150b5bd17cbbb96c5366e9c2d
Amadi <whytes2256@gmail.com>
2023-03-21 13:20:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+/*
+ * Return: On success 0.
+ */
+int main(void)
+{
+	_putchar('s');
+	_putchar('t');
+	_putchar('d');
+	_putchar('i');
+	_putchar('o');
+	_putchar('\n');
+
+	return (0);
+}
