d6e907aebee4b2e5c5e16be2cfcedb70a05d675f
Amadi <whytes2256@gmail.com>
2023-04-07 04:41:19 -0700

    fixes

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
   *_strlen_recursion: function that returns the length of a string
-  *@s: length of sting to be printed
+  *@*s: length of sting to be printed
   *Return: Always 0
   */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		(return 0);
-		(return 1 + length(s + 1));
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + (*s++);
+	}
 }
