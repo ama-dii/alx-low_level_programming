1af2fc64c6cd11d71d27ab531338f0b83efc957a
Amadi <whytes2256@gmail.com>
2023-04-14 05:09:24 -0700

    fixing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,8 +16,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1 && s1[len1])
 		len1++;
 
-	while (j < n && i < (len1 + n))
-		s[i++] = s2[j++];
+	while (s2 && s2[len2])
+		len2++;
 
 	if (n < len2)
 	{
