30f34d24f623d8bf53752e574acd86b493a4269d
Amadi <whytes2256@gmail.com>
2023-04-27 12:47:10 -0700

    created a 1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "list,h"
+
+/**
+  *list_len - funct that prints the member od a list
+  *@h: member of the list to be printed
+  *Return: 0
+**/
+
+size_t list_len(const list_t *h)
+{
+	unsigned int mem;
+
+	for (mem = 0; h !=NULL; mem++)
+	{
+		h = h->next;
+	}
+
+	return (mem);
+
+}
+
