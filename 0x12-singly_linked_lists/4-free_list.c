35c421c73afa7d8001e1134a6479d88f793fc8eb
Amadi <whytes2256@gmail.com>
2023-04-27 15:14:41 -0700

    created 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+  * free_list- funct that frees a list
+  * @head- pointer to pointer of head of linked list
+  * Return- nothing
+**/
+
+void free_list(list_t *head)
+{
+	list_t *temp_free;
+
+	for (temp_free = head; temp_free != NULL; temp_free = temp_free->next)
+	{
+		free(temp_free->str);
+		free(temp_free);
+	}
+}
