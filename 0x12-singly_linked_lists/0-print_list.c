ac54b02b217cab7bf054db773a06c17e21036964
Amadi <whytes2256@gmail.com>
2023-04-27 11:47:42 -0700

    created a 0-print_list.c file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/** print_list- func that prints the element of a list
+  * @h: elemnt of the list to be printed
+  * Return: the number of nodes
+**/
+
+size_t print_list(const list_t *h)
+{
+	int nlist;
+
+	for (nlist = 0; h != NULL; nlist++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+	}
+
+	return (nlist);
+}
+
