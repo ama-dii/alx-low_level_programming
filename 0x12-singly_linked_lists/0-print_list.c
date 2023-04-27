f14dc2758dde41aaf8155679d3ae98c09511cdae
Amadi <whytes2256@gmail.com>
2023-04-27 12:04:11 -0700

    error message in 0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
 
-/** print_list- func that prints the element of a list
+/**
+  *print_list- func that prints the element of a list
   * @h: elemnt of the list to be printed
   * Return: the number of nodes
 **/
@@ -10,7 +11,7 @@ size_t print_list(const list_t *h)
 {
 	int nlist;
 
-	for (nlist = 0; h != NULL; nlist++)
+	for (nlist = 0; h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -19,6 +20,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
+		nlist++;
 	}
 
 	return (nlist);
