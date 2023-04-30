90c9c4abcebb51b69972b9eb5e5b7fab588af342
Amadi <whytes2256@gmail.com>
2023-04-30 13:32:35 -0700

    created 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * add_nodeint- function that adds a new node at the beginning of a listint_t list.
+  * @h: linked list first nod
+  * Return: new element nod
+**/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	int num_nod;
+
+	for (num-nod = 0; h != NULL; num_nod++)
+	{
+		h = h->next;
+	}
+	return (num_nod);
+
+}
