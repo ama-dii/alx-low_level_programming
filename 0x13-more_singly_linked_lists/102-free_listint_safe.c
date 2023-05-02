aa1485ee5e53c4bad590eeaf6f44eb4530f4e518
Amadi <whytes2256@gmail.com>
2023-05-02 14:59:20 -0700

    error checks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,72 +1,38 @@
 #include "lists.h"
 
 /**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
+ * free_listint_safe - func that frees a linked list
+ * @h: pointer to the first node in the linked list
+ * Return: size of the list
  */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
 
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - frees a linked list.
- * @h: head of a list.
- *
- * Return: size of the list that was freed.
- */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
 
-	hptr = NULL;
-	while (*h != NULL)
-	{
-		new = malloc(sizeof(listp_t));
+size_t len_of_list = 0;
+int df;
+listint_t *T;
+if (!h || !*h)
+return (0);
+while (*h)
+{
+df = *h - (*h)->next;
+if (df > 0)
+{
+T = (*h)->next;
+free(*h);
+*h = T;
+len_of_list++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len_of_list++;
+break;
+}
+}
+*h = NULL;
+return (len_of_list);
 
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
-			}
-		}
-
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
-	}
-
-	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
 }
