a8e8176d412cf3d527caa950fa17e81972e20a31
Amadi <whytes2256@gmail.com>
2023-06-15 20:03:23 -0700

    fixinh error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,18 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *current;
-	unsigned int n; /* number of nodes */
+	const dlistint_t *ptr = h;
+	size_t n = 0;
 
-	current = h;
-	n = 0;
-	while (current != NULL)
+	while (ptr && ptr->prev)
+		ptr = ptr->prev;
+	while (ptr)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
+		printf("%d\n", ptr->n);
 		n++;
+		ptr = ptr->next;
 	}
+	return (n);
 
 	return (n);
 }
